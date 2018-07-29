2c29a0205ec15a3012d6bea6826157061e1562c4
Emily Welsch <emwelsch@gmail.com>
2018-07-29 23:29:02 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -70,11 +70,10 @@ def can_update_using_update_method
 end
 
 def can_update_multiple_items_at_once
-  # Change title of all movies to "A Movie"
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  Movie.collect do |movie|
+  Movie.all.each do |movie|
     movie.update(title: "A Movie")
   end
 end
